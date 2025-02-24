cd9f3ff1a505ed9734c803ccfdb724513b9941ca
A-Shermatov <azamatjon.shermatov@gmail.com>
2025-02-24 22:45:35 +0300

    added timezone for models on auth_service/models

--- auth_service/models.py
+++ auth_service/models.py
@@ -21,8 +21,8 @@ class User(Base):
     role = Column(Enum(Role), default=Role.student)
     password = Column(String(128), nullable=False)
     is_active = Column(Boolean(), default=True)
-    created_at = Column(DateTime(), default=datetime.datetime.now())
-    updated_at = Column(DateTime(), default=datetime.datetime.now())
+    created_at = Column(DateTime(), default=datetime.datetime.now(datetime.timezone.utc))
+    updated_at = Column(DateTime(), default=datetime.datetime.now(datetime.timezone.utc))
 
 
 class Token(Base):
@@ -33,4 +33,4 @@ class Token(Base):
     token_type = Column(String(50), nullable=False)
     secret_key = Column(String(50), nullable=False)
     algorithm = Column(String(20), nullable=False)
-    created_at = Column(DateTime(), default=datetime.datetime.now())
+    created_at = Column(DateTime(), default=datetime.datetime.now(datetime.timezone.utc))
