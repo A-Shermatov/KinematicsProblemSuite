bc4acdf9a10fd02a2c6a74821568e7dea1aadd1d
A-Shermatov <azamatjon.shermatov@gmail.com>
2025-03-01 22:09:00 +0300

    fixed some functions on auth_service/possibility file

--- auth_service/possibility.py
+++ auth_service/possibility.py
@@ -3,15 +3,12 @@ from typing import cast
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
-import models, schemas, database
-from schemas import TokenRefresh
+import models, database
 
 from utils import *
 
 router = APIRouter()
 
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "60"))
-TOKEN_TYPE = os.getenv("TOKEN_TYPE", "bearer")
 
 def get_db():
     db = database.SessionLocal()
@@ -26,24 +23,7 @@ def check_user(token: str, role: str, db):
 
 @router.post("/student")
 def student(token: str, db: Session = Depends(get_db)):
-    db_token = db.query(models.Token).filter(cast("ColumnElement[bool]", models.Token.token == token)).first()
-    data = decode_token(token)
-
-    if "error" in data and data["error"] == "ExpiredSignatureError":
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Token has expired"
-        )
-
-    if ("error" in data and data["error"] == "InvalidTokenError" or
-            db_token is None or
-            data is None or
-            'sub' not in data or
-            "exp" not in data):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid token"
-        )
+    pass
 
 
 @router.get("/teacher")
